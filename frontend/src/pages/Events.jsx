b2fc6fa38cf1aa0f4be64939058975f15b2a898d
Norrie <norriedy@proton.me>
2026-05-04 15:02:49 -0400

    more progress on event tabs & filters

--- frontend/src/pages/Events.jsx
+++ frontend/src/pages/Events.jsx
@@ -13,9 +13,15 @@ const eventCategories =
 //where all the tabs are stored
 function eventTabs()
 {
+    ///////CONSTS:
 
     const [activeCategory, setActiveCategory] = useState("All"); //used for setting active category when clicked
-    const [events, setEvents] = useState([]);
+    //(all is used by default, bc when we start up there's no filter selected yet)
+
+    const [events, setEvents] = useState([]); //used for setting events we see on screen
+    //(start off with nothing bc no events set yet) 
+
+    ///////FOR TAB BUTTONS: 
 
     //function that handles clicking and setting the active tab
     function activeCategory(item)
@@ -39,29 +45,57 @@ function eventTabs()
         }
     }
 
+    /////LOADING EVENTS:
+
+    
+    //loads the events we see when we choose which events to select/filter by
+    //(needs to be async as we need to wait for api calls !!!)
+    async function loadEvents()
+    {
+        const filtersAdded = {};  //we define empty obj for events loaded bc this essentially
+        //represents the filters we're adding; we start with no filters by default
+
+        if(activeCategory !== "All") //(adds category filter)
+        {
+            //if all the categories aren't active, we only show the categories in the selected category
+            filtersAdded.category = activeCategory;   
+        }
+
+        //TODO: add more filters (once other stuff is set up yippee)
+
+        //contact API to get all the events that match these filters, returned into list
+        const eventsLoaded = await api.get("/events", {filtersAdded});
+
+        //using events hook, pass in the events loaded that we jsut got from API and 
+        //pass in, loading the events onto the screen
+        setEvents(eventsLoaded);
+    }
+
+    //^now we call the loadEvents function up there using useEffect, so this triggers whenever we change activeCategory
+    useEffect(() => {loadEvents();}, [activeCategory]);
+    //(when activeCategory changes, call loadEvents, activeCategory is dependency)
+
+
+
     return 
     (
+        //all the tabs inside the filter bar
+        //TODO: is key supposed to be in brackets or quotations?
         <div id = "FilterBar">
-            //'All' tab
-            <button key={All}  onClick={setActiveCategory} className={isActive(All)}>All</button> 
+            <button key="All"  onClick={setActiveCategory} className={isActive(All)}>All</button> 
             
-            //'Parties and Pregames' tab
-            <button key={PartiesAndPregames}  onClick={setActiveCategory} 
+            <button key="PartiesAndPregames"  onClick={setActiveCategory} 
                 className={isActive(PartiesAndPregames)}>PartiesAndPregames</button>
-            
-            //'Sports and Tournaments' tab
-            <button key={SportsAndTournaments}  onClick={setActiveCategory} 
+
+            <button key="SportsAndTournaments"  onClick={setActiveCategory} 
                 className={isActive(SportsAndTournaments)}>SportsAndTournaments</button> 
 
-            //Study Sessions tab
-            <button key={StudySessions}  onClick={setActiveCategory} 
+            <button key="StudySessions"  onClick={setActiveCategory} 
                 className={isActive(StudySessions)}>Study Sessions</button>
             
-            //Miscellaneous tab
-            <button key={Miscellaneous}  onClick={setActiveCategory} className={isActive(Miscellaneous)}>Miscellaneous</button>
-            
-            //event list
+            <button key="Miscellaneous"  onClick={setActiveCategory} className={isActive(Miscellaneous)}>Miscellaneous</button>  
         </div>
+        //events list:
 
     );
 
