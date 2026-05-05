c921f24f844e6b40512fc2860e07475742906895
ayad alharbi <ayadalharbi232@gmail.com>
2026-05-05 03:12:51 +0100

    built EventCard.jsx

---
+++ frontend/src/components/EventCard.jsx
@@ -0,0 +1,37 @@
+import { useAuth } from "context/AuthContext"
+
+const EventCard = ({ event }) => {
+    const { user } = useAuth();
+
+    return (
+        <div className="event-card">
+            <h1>{event.name}</h1>
+
+            {/*When a function that returns a color given a category is implemented put in place of white*/}
+            <span className="category-badge" style={{ color: "white" }}>
+                {event.category}
+            </span>
+
+            {/*Location + time*/}
+            <div className="location-time">
+                <p>Location: {event.location} </p>
+                <p>Time: {event.time} </p>
+            </div>
+
+
+            {/*RSVP Count / RSVP Capacity*/}
+            <div className="rsvp">
+                <p>RSVP: {event.rsvpCount}/{event.rsvpCap} </p>
+            </div>
+
+            {/*Host info*/}
+            <div className="host-info">
+                <p>
+                    Hosted by: {user.name} {user.rankBadge}
+                </p>
+            </div>
+
+        </div>
+    )
+
+}
